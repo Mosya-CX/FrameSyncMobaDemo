de761e30e1256e954cabe7710f1207c55b09ab68
Mosya <1906848521@qq.com>
2026-03-10 21:21:11 +0800

    5

--- Assets/Scripts/Ability/AbilityBaseCondition.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class AbilityBaseCondition : ScriptableObject
-{
-    public abstract bool CanTrigger(AbilityInfo abilityInfo, in InputInfo inputInfo);
-    public abstract void PayAbilityCost(AbilityInfo abilityInfo);
-    public abstract void CancelReturn(AbilityInfo abilityInfo);
-}
-
--- Assets/Scripts/AbilityIndicator/AbilityIndicatorBase.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class AbilityIndicatorBase : ScriptableObject
-{
-    public abstract void OnCreate(AbilityIndicatorRuntime runtimeData);
-    public abstract void ActiveIndicator(AbilityIndicatorRuntime runtimeData);
-    public abstract void UpdateIndicator(AbilityIndicatorRuntime runtimeData);
-    public abstract void InactiveIndicator(AbilityIndicatorRuntime runtimeData);
-}
